a917bc1bca53e12673c6c61e36008676c82179df
Alonso Baldioceda <130702868+alonsobs@users.noreply.github.com>
2023-07-11 13:11:44 -0600

    Testimonials

--- src/components/CardV4.jsx
+++ src/components/CardV4.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { prefix } from "../shared/styles.js";
+import styled from "styled-components";
 // import { GatsbyImage } from "gatsby-plugin-image";
+
+// Components
+import RichText from "../components/RichText";
+
+// Variables
+import { prefix, colors } from "../shared/styles.js";
+
+// Styles
+const StyledCardV4 = styled.div`
+  P {
+    color: #fff;
+    text-align: center;
+  }
+
+  h5 {
+    /* background-color: ${colors.sycamore}; */
+    border-bottom: 4px solid ${colors.sycamore};
+    padding: 6px 10px;
+  }
+`;
 
 const CardV4 = ({ text, image, alt, name, position }) => {
   return (
-    <div
+    <StyledCardV4
       className={`d-flex justify-content-center flex-column w-75 mx-auto ${prefix}-card-v4`}
     >
-      <p className="text-white text-center mb-5">{text}</p>
+      <RichText text={text} />
       <div className="d-flex align-items-center flex-column">
         {/* <span className="mb-3">
           {image ? (
@@ -18,10 +38,10 @@ const CardV4 = ({ text, image, alt, name, position }) => {
             />
           ) : null}
         </span> */}
-        <h6 className="mb-0 text-white">{name}</h6>
-        <p className="mb-3 text-white">{position}</p>
+        <h5 className="mb-2 text-white name">{name}</h5>
+        <p className="mb-0 text-white">{position}</p>
       </div>
-    </div>
+    </StyledCardV4>
   );
 };
 
